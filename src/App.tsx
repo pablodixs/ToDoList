f6cd966c9864f8afd5c4aa72ca4f82b338018400
Pablo Dias <pabllosoarez@gmail.com>
2022-09-22 11:59:29 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+
+import './styles/global.css';
+import { TextArea } from './components/TextArea';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <TextArea />
+    </div>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './style.module.css'
+
+import logo from '../../assets/logo.svg'
+
+export function Header() {
+   return(
+      <header className={styles.header}>
+         <img className={styles.logo} src={logo} />
+      </header>
+   )
+}
